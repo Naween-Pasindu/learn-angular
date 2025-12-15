8a06ae2ec5a4614510b7ec176325cf1221af8584
Naween Pasindu <htnaweenpasindu@gmail.com>
2025-12-15 23:15:54 +0530

    pipes

--- src/app/components/todo-item-component/todo-item-component.component.ts
+++ src/app/components/todo-item-component/todo-item-component.component.ts
@@ -1,10 +1,11 @@
 import { Component, input, output } from '@angular/core';
 import { Todo } from '../../model/todo.type';
 import { HighlightCompletedTodoDirective } from '../../directives/highlight-completed-todo.directive';
+import { UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-todo-item-component',
-  imports: [HighlightCompletedTodoDirective],
+  imports: [HighlightCompletedTodoDirective, UpperCasePipe],
   templateUrl: './todo-item-component.component.html',
   styleUrl: './todo-item-component.component.scss'
 })
